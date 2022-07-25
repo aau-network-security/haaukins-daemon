8fe3fbd8ab525fade7dd0610f8ccbc07534bc95e
Mikkel Christiansen <mikkel.christiansen250996@gmail.com>
2022-07-25 16:02:47 +0200

    Moved db connection conf to database package

--- internal/daemon/models.go
+++ internal/daemon/models.go
@@ -1,12 +1,15 @@
 package daemon
 
+import "github.com/aau-network-security/haaukins-daemon/internal/database"
+
 type Config struct {
-	Port       uint          `yaml:"port"`
-	Database   ServiceConfig `yaml:"db-config,omitempty"`
-	Production bool          `yaml:"prodmode,omitempty"`
-	jwtSecret  string        `yaml:"jwtSecret,omitempty"`
-	Rechaptcha string        `yaml:"recaptcha-key,omitempty"`
-	APICreds   APICreds      `yaml:"api-creds,omitempty"`
+	Host       string            `json:"host"`
+	Port       uint              `yaml:"port"`
+	Database   database.DbConfig `yaml:"db-config,omitempty"`
+	Production bool              `yaml:"prodmode,omitempty"`
+	JwtSecret  string            `yaml:"jwtSecret,omitempty"`
+	Rechaptcha string            `yaml:"recaptcha-key,omitempty"`
+	APICreds   APICreds          `yaml:"api-creds,omitempty"`
 }
 
 type APICreds struct {
@@ -14,13 +17,9 @@ type APICreds struct {
 	Password string `yaml:"password,omitempty"`
 }
 
+type APIResponse struct {
+	Status string `yaml:"status,omitempty"`
+}
+
 // VPNConnConf includes configuration
 // information for gRPC client on VPN service
-
-type ServiceConfig struct {
-	Host     string `yaml:"host"`
-	Port     uint64 `yaml:"port"`
-	DbName   string `yaml:"db_name"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
-}
