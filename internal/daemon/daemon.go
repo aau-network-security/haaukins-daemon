dde77217e222d55f872dacf9f0010675e8a7e9ca
Mikkel Christiansen <mikkel.christiansen250996@gmail.com>
2022-07-25 16:00:18 +0200

    Fixed jwt secret error, daemon now initially connects to all exercise dbs in haaukins db. If succesfully connected they will be placed in a hashtable

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -1,16 +1,21 @@
 package daemon
 
 import (
+	"context"
 	"errors"
 	"io/ioutil"
 
 	"github.com/aau-network-security/haaukins-daemon/internal/database"
+	eproto "github.com/aau-network-security/haaukins-daemon/internal/exercise/ex-proto"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v2"
 )
 
-type dbHandler struct {
-	db *database.Queries
+type daemon struct {
+	conf      *Config
+	db        *database.Queries
+	exClients map[string]eproto.ExerciseStoreClient
 }
 
 func NewConfigFromFile(path string) (*Config, error) {
@@ -25,10 +30,7 @@ func NewConfigFromFile(path string) (*Config, error) {
 		return nil, err
 	}
 
-	// todo: replace all if statement with something better
-	// change the way of handling configuration files
-
-	if c.jwtSecret == "" {
+	if c.JwtSecret == "" {
 		return nil, errors.New("missing signing key in configuration")
 	}
 
@@ -55,28 +57,61 @@ func NewConfigFromFile(path string) (*Config, error) {
 	return &c, nil
 }
 
-func startAPI() {
-	//log.Info("Starting API")
-	r := gin.Default()
-	// Setting up db connection
-	db, err := database.InitConn()
+func New(conf *Config) (*daemon, error) {
+	ctx := context.Background()
+	log.Info().Msg("Creating daemon...")
+	db, err := conf.Database.InitConn()
 	if err != nil {
-		//log.Fatal("[NBI2] Failed to connect to database: ", err)
+		log.Fatal().Err(err).Msg("[Haaukins-daemon] Failed to connect to database")
+		return nil, err
 	}
-	// Getting crmclient params
-
-	dh := dbHandler{
-		db: db,
+	// Getting exercise database connections stored in the database
+	exersiceDatabases, err := db.GetExerciseDatabases(ctx)
+	if err != nil {
+		log.Fatal().Err(err).Msg("[Haaukins-daemon] Failed to get currently connected exercise databases")
+		return nil, err
 	}
 
-	dh.setupRouters(r)
-	r.Run(":8080")
+	// Using a hashtable for exercise database connections
+	// If the database name is not in the hashtable we know that the database is not connected
+	log.Info().Msg("Connecting to currently stored exercise databases...")
+	exClients := make(map[string]eproto.ExerciseStoreClient)
+	for _, exDb := range exersiceDatabases {
+		exDbConfig := ServiceConfig{
+			Grpc:    exDb.Url.String,
+			AuthKey: exDb.AuthKey.String,
+			SignKey: exDb.SignKey.String,
+			Enabled: true,
+		}
+		exClient, err := NewExerciseClientConn(exDbConfig)
+		if err != nil {
+			log.Warn().Err(err).Msgf("[exercise-service]: error on creating gRPC communication")
+
+		} else {
+			exClients[exDb.Name.String] = exClient
+			log.Debug().Str("Url", exDbConfig.Grpc).Msg("Exercise service connected !")
+		}
+	}
+
+	d := &daemon{
+		conf:      conf,
+		db:        db,
+		exClients: exClients,
+	}
+	return d, nil
 }
 
-func (dh *dbHandler) setupRouters(r *gin.Engine) {
+func (d *daemon) Run() error {
+
+	r := gin.Default()
+	d.setupRouters(r)
+	return r.Run(":8080")
+}
+
+func (d *daemon) setupRouters(r *gin.Engine) {
 	admin := r.Group("/api/v1/admin")
 	event := r.Group("/api/v1/event")
 
-	dh.adminSubrouter(admin)
-	dh.eventSubrouter(event)
+	d.adminSubrouter(admin)
+	d.eventSubrouter(event)
 }
