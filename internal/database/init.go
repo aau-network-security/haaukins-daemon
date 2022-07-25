82d679c2b7fc240f942313f3f1ec35925ea4d453
Mikkel Christiansen <mikkel.christiansen250996@gmail.com>
2022-07-25 16:03:27 +0200

    Moved db connection conf to database package, and added a ping to check for database connection

--- internal/database/init.go
+++ internal/database/init.go
@@ -3,20 +3,28 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"os"
 
 	_ "github.com/lib/pq"
 )
 
-func InitConn() (*Queries, error) {
+type DbConfig struct {
+	Host     string `yaml:"host"`
+	Port     uint64 `yaml:"port"`
+	DbName   string `yaml:"db_name"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+func (conf *DbConfig) InitConn() (*Queries, error) {
 	driver := "postgres"
-	host := os.Getenv("DB_HOST")
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	connectstring := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, username, password, dbname)
+	connectstring := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", conf.Host, conf.Username, conf.Password, conf.DbName)
+	// Setting up the database connection
 	conn, err := sql.Open(driver, connectstring)
 	if err != nil {
+		return nil, err
+	}
+	// Pinging the connection to make sure that the db is alive
+	if err := conn.Ping(); err != nil {
 		return nil, err
 	}
 	db := New(conn)
