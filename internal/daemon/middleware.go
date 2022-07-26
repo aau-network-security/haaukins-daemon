4d5df9774d10a695fa6f87151b6511bef6a43db2
Mikkel Christiansen <mikkel.christiansen250996@gmail.com>
2022-07-26 15:32:11 +0200

    Added Cors middleware, added claims to gin context in auth middleware, added access to daemon config for jwt secret

--- internal/daemon/middleware.go
+++ internal/daemon/middleware.go
@@ -4,29 +4,28 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"os"
+	"time"
 
+	"github.com/aau-network-security/haaukins-daemon/internal/database"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 )
 
 // todo Need to create different tokens based on it is an admin login or participant login
-func createAdminToken() (string, error) {
-	// var err error
-	// atClaims := jwt.MapClaims{}
-	// atClaims["authorized"] = true
-	// atClaims["username"] = user.Username
-	// atClaims["role"] = user.Role
-	// atClaims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-	// atClaims["email"] = user.Email
-	// at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	// token, err := at.SignedString([]byte(SECRET_KEY))
-	// if err != nil {
-	// 	return "", err
-	// }
-	// return token, nil
-	return "token", nil
+func (d *daemon) createAdminToken(user database.AdminUser) (string, error) {
+	atClaims := jwt.MapClaims{}
+	atClaims["username"] = user.Username
+	atClaims["role"] = user.RoleID
+	atClaims["organization"] = user.OrganizationID
+	atClaims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	atClaims["email"] = user.Email
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(d.conf.JwtSecret))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
 
 func createParticipantToken() (string, error) {
@@ -46,21 +45,19 @@ func createParticipantToken() (string, error) {
 	return "token", nil
 }
 
-func jwtExtract(c *gin.Context) string {
+func (d *daemon) jwtExtract(c *gin.Context) string {
 	token := c.GetHeader("Authorization")
-	var SECRET_KEY = os.Getenv("SECRET_KEY")
-	log.Debug().Msgf("Using secret key: ", SECRET_KEY)
+	log.Debug().Msgf("Using secret key: %s", d.conf.JwtSecret)
 	return token
 }
 
-func jwtVerify(c *gin.Context) (*jwt.Token, error) {
-	tokenString := jwtExtract(c)
-	var SECRET_KEY = os.Getenv("SECRET_KEY")
+func (d *daemon) jwtVerify(c *gin.Context) (*jwt.Token, error) {
+	tokenString := d.jwtExtract(c)
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
-		return []byte(SECRET_KEY), nil
+		return []byte(d.conf.JwtSecret), nil
 	})
 	if err != nil {
 		return nil, err
@@ -68,8 +65,8 @@ func jwtVerify(c *gin.Context) (*jwt.Token, error) {
 	return token, nil
 }
 
-func jwtValidate(c *gin.Context) (jwt.MapClaims, error) {
-	token, err := jwtVerify(c)
+func (d *daemon) jwtValidate(c *gin.Context) (jwt.MapClaims, error) {
+	token, err := d.jwtVerify(c)
 	if err != nil {
 		return nil, err
 	}
@@ -82,16 +79,34 @@ func jwtValidate(c *gin.Context) (jwt.MapClaims, error) {
 	}
 }
 
-func tokenAuthMiddleware() gin.HandlerFunc {
+func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//enableCors(&w)
-		/*if r.Method == "OPTIONS" {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
 			return
-		}*/
-		_, err := jwtValidate(c)
+		}
+
+		c.Next()
+	}
+}
+
+func (d *daemon) adminAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		claims, err := d.jwtValidate(c)
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, APIResponse{Status: "Invalid JWT"})
 			return
 		}
+		// Passing jwt claims to next handler function
+		c.Set("username", claims["username"])
+		c.Set("email", claims["email"])
+		c.Set("organization", claims["organization"])
+		c.Set("role", claims["role"])
+		c.Next()
 	}
 }
