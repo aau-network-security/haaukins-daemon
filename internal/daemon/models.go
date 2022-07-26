95b95ffff8bffe8666c2522adcff13250ed90b0d
Mikkel Christiansen <mikkel.christiansen250996@gmail.com>
2022-07-26 15:32:26 +0200

    Added logging config

--- internal/daemon/models.go
+++ internal/daemon/models.go
@@ -3,13 +3,22 @@ package daemon
 import "github.com/aau-network-security/haaukins-daemon/internal/database"
 
 type Config struct {
-	Host       string            `json:"host"`
+	Host       string            `yaml:"host"`
 	Port       uint              `yaml:"port"`
+	AuditLog   Logging           `yaml:"auditLog"`
 	Database   database.DbConfig `yaml:"db-config,omitempty"`
 	Production bool              `yaml:"prodmode,omitempty"`
 	JwtSecret  string            `yaml:"jwtSecret,omitempty"`
 	Rechaptcha string            `yaml:"recaptcha-key,omitempty"`
 	APICreds   APICreds          `yaml:"api-creds,omitempty"`
+}
+
+type Logging struct {
+	Directory  string `yaml:"directory"`
+	FileName   string `yaml:"fileName"`
+	MaxBackups int    `yaml:"max-backups"`
+	MaxSize    int    `yaml:"max-size"`
+	MaxAge     int    `yaml:"max-age"`
 }
 
 type APICreds struct {
@@ -18,7 +27,8 @@ type APICreds struct {
 }
 
 type APIResponse struct {
-	Status string `yaml:"status,omitempty"`
+	Status string `json:"status,omitempty"`
+	Token  string `json:"token,omitempty"`
 }
 
 // VPNConnConf includes configuration
