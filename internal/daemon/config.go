0fc2cff1cdb597be9dac36799974fc6f1bded7dc
Mikkel Christiansen <mikkel.christiansen250996@gmail.com>
2022-07-27 14:09:53 +0200

    Moved around some types

---
+++ internal/daemon/config.go
@@ -0,0 +1,27 @@
+package daemon
+
+import "github.com/aau-network-security/haaukins-daemon/internal/database"
+
+type Config struct {
+	Host       string            `yaml:"host"`
+	Port       uint              `yaml:"port"`
+	AuditLog   Logging           `yaml:"auditLog"`
+	Database   database.DbConfig `yaml:"db-config,omitempty"`
+	Production bool              `yaml:"prodmode,omitempty"`
+	JwtSecret  string            `yaml:"jwtSecret,omitempty"`
+	Rechaptcha string            `yaml:"recaptcha-key,omitempty"`
+	APICreds   APICreds          `yaml:"api-creds,omitempty"`
+}
+
+type Logging struct {
+	Directory  string `yaml:"directory"`
+	FileName   string `yaml:"fileName"`
+	MaxBackups int    `yaml:"max-backups"`
+	MaxSize    int    `yaml:"max-size"`
+	MaxAge     int    `yaml:"max-age"`
+}
+
+type APICreds struct {
+	Username string `yaml:"username,omitempty"`
+	Password string `yaml:"password,omitempty"`
+}
--- internal/daemon/models.go
+++ internal/daemon/models.go
@@ -1,29 +1,16 @@
 package daemon
 
-import "github.com/aau-network-security/haaukins-daemon/internal/database"
-
-type Config struct {
-	Host       string            `yaml:"host"`
-	Port       uint              `yaml:"port"`
-	AuditLog   Logging           `yaml:"auditLog"`
-	Database   database.DbConfig `yaml:"db-config,omitempty"`
-	Production bool              `yaml:"prodmode,omitempty"`
-	JwtSecret  string            `yaml:"jwtSecret,omitempty"`
-	Rechaptcha string            `yaml:"recaptcha-key,omitempty"`
-	APICreds   APICreds          `yaml:"api-creds,omitempty"`
-}
-
-type Logging struct {
-	Directory  string `yaml:"directory"`
-	FileName   string `yaml:"fileName"`
-	MaxBackups int    `yaml:"max-backups"`
-	MaxSize    int    `yaml:"max-size"`
-	MaxAge     int    `yaml:"max-age"`
-}
-
-type APICreds struct {
-	Username string `yaml:"username,omitempty"`
-	Password string `yaml:"password,omitempty"`
+type AdminClaims struct {
+	Username       string `json:"username"`
+	Email          string `json:"email"`
+	OrganizationID int32  `json:"organization_id"`
+	RoleID         int32  `json:"role_id"`
+	WriteAll       bool   `json:"write_all"`
+	ReadAll        bool   `json:"read_all"`
+	WriteLocal     bool   `json:"write_local"`
+	ReadLocal      bool   `json:"read_local"`
+	Jti            string `json:"jti"`
+	Exp            int64  `json:"exp"`
 }
 
 type APIResponse struct {
