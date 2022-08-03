fb990bcdc8f424ebc6ae1908223e5e45cfa9b63a
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2022-08-03 14:13:25 +0200

    No longer use for lower

--- internal/daemon/helpers.go
+++ internal/daemon/helpers.go
@@ -1,14 +1,12 @@
 package daemon
 
 import (
-	"strings"
-
 	"github.com/gin-gonic/gin"
 )
 
 func unpackAdminClaims(c *gin.Context) AdminClaims {
 	return AdminClaims{
-		Username:     strings.ToLower((c.MustGet("sub").(string))),
+		Username:     string(c.MustGet("sub").(string)),
 		Email:        string(c.MustGet("email").(string)),
 		Organization: string(c.MustGet("organization").(string)),
 		Role:         string(c.MustGet("role").(string)),
