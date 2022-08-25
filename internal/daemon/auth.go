8bcb9f42fc8fbd642c3c4372726b816b739a887b
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2022-08-25 15:06:07 +0200

    Removed outcommented section

--- internal/daemon/auth.go
+++ internal/daemon/auth.go
@@ -4,26 +4,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// func authOrganizationAccess(admin AdminClaims, orgToAccess int32) bool {
-// 	if admin.OrganizationID != orgToAccess {
-// 		return false
-// 	} else {
-// 		return true
-// 	}
-// }
-
-// func authRoleAccess(admin AdminClaims, role database.Role) bool {
-// 	if role.WriteAll && !admin.WriteAll {
-// 		return false
-// 	}
-
-// 	if role.ReadAll && !admin.ReadAll {
-// 		return false
-// 	}
-
-// 	return true
-// }
-
 func verifyPassword(hash, password string) bool {
 	byteHash := []byte(hash)
 	bytePassword := []byte(password)
