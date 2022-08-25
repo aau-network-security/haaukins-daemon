2447e77d8ade0560fcd426a9f89b4ffbf6de6594
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2022-08-25 15:06:56 +0200

    Expanded daemon struct, connect to all agents on startup

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -1,18 +1,22 @@
 package daemon
 
 import (
+	"context"
 	"errors"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
+	"github.com/aau-network-security/haaukins-daemon/internal/agent"
 	"github.com/aau-network-security/haaukins-daemon/internal/database"
 	eproto "github.com/aau-network-security/haaukins-daemon/internal/exercise/ex-proto"
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v2"
@@ -22,8 +26,12 @@ type daemon struct {
 	conf        *Config
 	db          *database.Queries
 	exClient    eproto.ExerciseStoreClient
+	agentPool   *agent.AgentPool
 	auditLogger *zerolog.Logger
 	enforcer    *casbin.Enforcer
+	cache       *redis.Client
+	eventpool   *eventPool
+	m           sync.RWMutex
 }
 
 const (
@@ -111,20 +119,52 @@ func NewConfigFromFile(path string) (*Config, error) {
 }
 
 func New(conf *Config) (*daemon, error) {
-	//ctx := context.Background()
+	ctx := context.Background()
 	log.Info().Msg("Creating daemon...")
 	// TODO rewrte init function if filtered adapter is used
+	//Setting up database connection
 	db, gormDb, err := conf.Database.InitConn()
 	if err != nil {
 		log.Fatal().Err(err).Msg("[Haaukins-daemon] Failed to connect to database")
 	}
 
+	// Connecting to the exercise service
+	log.Info().Msg("connecting to exercise service")
 	exClient, err := NewExerciseClientConn(conf.ExerciseService)
 	if err != nil {
 		log.Fatal().Err(err).Msgf("[exercise-service]: error on creating gRPC communication")
 
 	}
 
+	// Connecting to all haaukins agents
+	log.Info().Msg("Connecting to haaukins agents...")
+	agentsInDb, err := db.GetHaaukinsAgents(ctx)
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not get haaukins agents from database")
+	}
+	agents := make(map[string]agent.HaaukinsAgent)
+	for _, a := range agentsInDb {
+		agentConfig := ServiceConfig{
+			Grpc:       a.Url,
+			AuthKey:    a.AuthKey,
+			SignKey:    a.SignKey,
+			TLSEnabled: a.Tls,
+		}
+		client, err := NewAgentClientConnection(agentConfig)
+		if err != nil {
+			log.Warn().Err(err).Msg("error connecting to agent at url: " + agentConfig.Grpc)
+		} else {
+			var agentToAdd = agent.HaaukinsAgent{
+				Client:   client,
+				Capacity: a.Capacity,
+				CapUsed:  0,
+			}
+			agents[a.Name] = agentToAdd
+		}
+	}
+	agentPool := agent.AgentPool{
+		Agents: agents,
+	}
 	// dataSource := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", conf.Database.Host, conf.Database.Username, conf.Database.Password, conf.Database.DbName, conf.Database.Port)
 	// adapter, err := gormadapter.NewFilteredAdapter("postgres", dataSource, true)
 	// if err != nil {
@@ -149,6 +189,7 @@ func New(conf *Config) (*daemon, error) {
 	// 	log.Fatal().Err(err).Msg("Error loading policies")
 	// }
 
+	// Inserting default casbin policies if they don't already exist
 	for _, p := range defaultPolicies {
 		if !enforcer.HasPolicy(p) {
 			if _, err := enforcer.AddPolicy(p); err != nil {
@@ -171,13 +212,14 @@ func New(conf *Config) (*daemon, error) {
 		}
 	}
 
-	// Creating audit logger to log admin events seperately
+	// Creating audit logger to log admin events seperately in a file
 	auditLogger := zerolog.New(newRollingFile(conf)).With().Logger()
 
 	d := &daemon{
 		conf:        conf,
 		db:          db,
 		exClient:    exClient,
+		agentPool:   &agentPool,
 		auditLogger: &auditLogger,
 		enforcer:    enforcer,
 	}
