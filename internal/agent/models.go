4752f2007aa871da0c7a48f455becee5686743c8
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2022-08-25 15:04:17 +0200

    Started implementing agent logic

---
+++ internal/agent/models.go
@@ -0,0 +1,33 @@
+package agent
+
+import (
+	"sync"
+
+	aproto "github.com/aau-network-security/haaukins-daemon/internal/agent/proto"
+)
+
+type AgentPool struct {
+	M      sync.RWMutex
+	Agents map[string]HaaukinsAgent
+}
+
+type HaaukinsAgent struct {
+	Client    aproto.AgentClient
+	Capacity  int32
+	CapUsed   int32
+	Heartbeat string
+	Errors    []error
+}
+
+type Lab struct {
+	tag         string
+	guacPort    uint
+	parentAgent string
+	isVPN       bool
+	exercises   map[string]Exercise
+}
+
+type Exercise struct {
+	tag             string
+	containerStatus map[string]uint
+}
