824758e083eadfe5d5ea149dc70ff375f17412d2
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2023-01-04 15:14:27 +0100

    Added team to event in eventpool, putting team inside queue if beginner event

--- internal/daemon/eventTeams.go
+++ internal/daemon/eventTeams.go
@@ -101,7 +101,7 @@ func (d *daemon) teamSignup(c *gin.Context) {
 		return
 	}
 
-	event, err := d.db.GetEventByTag(ctx, req.EventTag)
+	dbEvent, err := d.db.GetEventByTag(ctx, req.EventTag)
 	if err != nil {
 		log.Error().Err(err).Msg("error getting event by from database")
 		if err == sql.ErrNoRows {
@@ -112,7 +112,16 @@ func (d *daemon) teamSignup(c *gin.Context) {
 		return
 	}
 
-	if event.Secretkey != "" && req.SecretKey != event.Secretkey {
+	// Check if event is in event pool
+	// If event is in db but not in pool something is wrong
+	event, err := d.eventpool.GetEvent(req.EventTag)
+	if err != nil {
+		log.Error().Err(err).Msg("error getting event from eventpool")
+		c.JSON(http.StatusInternalServerError, APIResponse{Status: "internal server error"})
+		return
+	}
+
+	if dbEvent.Secretkey != "" && req.SecretKey != dbEvent.Secretkey {
 		c.JSON(http.StatusUnauthorized, APIResponse{Status: "invalid secretkey"})
 		return
 	}
@@ -129,7 +138,7 @@ func (d *daemon) teamSignup(c *gin.Context) {
 		Password:  string(pwHash),
 		Email:     req.Email,
 		Tag:       uuid.New().String()[0:8],
-		EventID:   event.ID,
+		EventID:   dbEvent.ID,
 		CreatedAt: time.Now(),
 		LastAccess: sql.NullTime{
 			Valid: false,
@@ -139,7 +148,7 @@ func (d *daemon) teamSignup(c *gin.Context) {
 	// Check if team for that event already exists
 	arg := db.CheckIfTeamExistsForEventParams{
 		Username: req.Username,
-		Eventid:  event.ID,
+		Eventid:  dbEvent.ID,
 	}
 	teamExists, err := d.db.CheckIfTeamExistsForEvent(ctx, arg)
 	if err != nil {
@@ -160,17 +169,38 @@ func (d *daemon) teamSignup(c *gin.Context) {
 		return
 	}
 
-	team := db.Team{
+	dbTeam := db.Team{
 		Username: newTeam.Username,
 		Email:    newTeam.Email,
 	}
-	token, err := d.createParticipantToken(ctx, team, req.EventTag)
+	token, err := d.createParticipantToken(ctx, dbTeam, req.EventTag)
 	if err != nil {
 		log.Error().Err(err).Msg("Error creating token")
 		c.JSON(http.StatusInternalServerError, APIResponse{Status: "Internal server error"})
 		return
 	}
 
-	// TODO Add to TeamsWaitingForLabs queue if beginner type event
+	team := &Team{
+		Username: req.Username,
+		Email:    req.Email,
+		Lab:      nil,
+	}
+	if err := event.AddTeam(team); err != nil {
+		log.Error().Err(err).Msg("error adding team to eventpool")
+	}
+
+	if EventType(dbEvent.Type) == TypeBeginner {
+		// Put team into waitingForLabs Queue if beginner type event
+		go func() {
+			defer func() {
+				if recover() != nil {
+					log.Debug().Msg("channel closed while sending team to queue")
+				}
+			}()
+			log.Info().Str("username", team.Username).Msg("putting team into queue for beginner lab")
+			event.TeamsWaitingForBrowserLabs <- team
+			log.Info().Str("username", team.Username).Msg("team got taken out of queue, exiting go routine")
+		}()
+	}
 	c.JSON(http.StatusOK, APIResponse{Status: "OK", Token: token})
 }
