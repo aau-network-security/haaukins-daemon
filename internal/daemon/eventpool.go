da1b2b89ff05848b308b6a6cf9eee0c4fab56353
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2023-01-04 15:12:26 +0100

    implemented team into eventpool, split lab and team queues into browser and vpn queues for each of them, added queue handlers

--- internal/daemon/eventpool.go
+++ internal/daemon/eventpool.go
@@ -1,6 +1,10 @@
 package daemon
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+)
 
 // Adds an event to the event pool
 func (ep *EventPool) AddEvent(event *Event) {
@@ -12,17 +16,121 @@ func (ep *EventPool) AddEvent(event *Event) {
 
 // Removes an event from the event pool
 // TODO make sure to close channels
-func (ep *EventPool) RemoveEvent(tag string) error {
+func (ep *EventPool) RemoveEvent(eventTag string) error {
 	ep.M.Lock()
 	defer ep.M.Unlock()
 
-	event, ok := ep.Events[tag]
+	event, ok := ep.Events[eventTag]
 	if !ok {
-		return fmt.Errorf("could not find event with tag: %s ", tag)
+		return fmt.Errorf("could not find event with tag: %s ", eventTag)
 	}
-	close(event.TeamsWaitingForLabs)
-	close(event.UnassignedLabs)
+	close(event.TeamsWaitingForBrowserLabs)
+	close(event.TeamsWaitingForVpnLabs)
+	close(event.UnassignedBrowserLabs)
+	close(event.UnassignedVpnLabs)
 
-	delete(ep.Events, tag)
+	delete(ep.Events, eventTag)
+	return nil
+}
+
+func (ep *EventPool) GetEvent(eventTag string) (*Event, error) {
+	ep.M.RLock()
+	defer ep.M.RUnlock()
+
+	event, ok := ep.Events[eventTag]
+	if !ok {
+		return nil, fmt.Errorf("could not find event with tag: %s ", eventTag)
+	}
+
+	return event, nil
+}
+
+// Event
+func (event *Event) GetTeam(username string) (*Team, error) {
+	event.M.RLock()
+	defer event.M.RUnlock()
+
+	team, ok := event.Teams[username]
+	if !ok {
+		return nil, fmt.Errorf("could not find team with username: %s ", username)
+	}
+
+	return team, nil
+}
+
+func (event *Event) AddTeam(team *Team) error {
+	event.M.Lock()
+	defer event.M.Unlock()
+
+	event.Teams[team.Username] = team
+
+	return nil
+}
+
+// The queue handlers are pulling out teams waiting for labs from one channel
+// Then waits for a lab to become available
+// When a labs enters the unassigned lab queue, it will assign the lab to the team previously pulled
+// Relies heavily on the blocking functionality of channels
+func (event *Event) startQueueHandlers() {
+	browserQueueHandler := func() {
+		for {
+			log.Debug().Msg("Waiting for teams to enter browser lab queue")
+			team, ok := <-event.TeamsWaitingForBrowserLabs
+			if ok {
+				log.Debug().Msgf("pulled team from browser queue: %v", team)
+			} else {
+				log.Debug().Msg("channel closed closing browserQueueHandler")
+				return
+			}
+
+			lab, ok := <-event.UnassignedBrowserLabs
+			if ok {
+				log.Debug().Msgf("pulled lab from browser queue: %v", lab)
+			} else {
+				log.Debug().Msg("channel closed closing browserQueueHandler")
+				return
+			}
+
+			// TODO Assign labs but first implement correct object to be sent between agent and daemon
+		}
+	}
+
+	vpnQueueHandler := func() {
+		for {
+			log.Debug().Msg("Waiting for team to enter vpn lab queue")
+
+			team, ok := <-event.TeamsWaitingForVpnLabs
+			if ok {
+				log.Debug().Msgf("pulled team from vpn queue: %v", team)
+			} else {
+				log.Debug().Msg("channel closed closing vpnQueueHandler")
+				return
+			}
+
+			lab, ok := <-event.UnassignedVpnLabs
+			if ok {
+				log.Debug().Msgf("pulled lab from vpn queue: %v", lab)
+			} else {
+				log.Debug().Msg("channel closed closing vpnQueueHandler")
+				return
+			}
+
+			// TODO Assign labs but first implement correct object to be sent between agent and daemon
+
+		}
+	}
+
+	go browserQueueHandler()
+
+	go vpnQueueHandler()
+}
+
+// Team
+
+func (team *Team) AddLab(lab *AgentLab) error {
+	team.M.Lock()
+	defer team.M.Unlock()
+
+	team.Lab = lab
 	return nil
 }
