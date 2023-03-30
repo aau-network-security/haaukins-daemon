5bc1c27a5286f2aea55fe343db26ed922c8e6c72
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2023-03-30 12:04:36 +0200

    Optimized distribution algorithm for simple events. Needs more optimization in regards to normalization of weights.

--- internal/daemon/agentpool.go
+++ internal/daemon/agentpool.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"math"
+	"sort"
 	"sync"
 	"time"
 
@@ -265,6 +265,7 @@ func (ap *AgentPool) createNewEnvOnAvailableAgents(ctx context.Context, eventPoo
 				log.Debug().Str("agentName", agent).Bool("agentFull", distribution.full).Int32("initialLabs", distribution.initialLabs).Msg("Initial labs for agent")
 			}
 
+			// Start an environment on each available agent
 			for _, availableAgent := range agentsAvailable {
 				envConfig := aproto.CreatEnvRequest{
 					EventTag: eventConfig.Tag,
@@ -319,172 +320,83 @@ type agentLabDistribution struct {
 	full        bool
 }
 
-// calculateLabDistribution returns a map of type agentLabDistribution, the key is the agentName, the main value is the initialLabs
-// It works by using the weights decided by whoever setup the agents to determine how many labs each agent should spin up
-//
-// It does not only take weights into account, it also takes into account the estimated memory usage, and if the amount of labs
-// will go above the threshold the remaining labs will be put into the labsRemainingToBeDistributed variable count.
-//
-// The algorithm will run in rounds, first trying to distribute the labs by weight. If all agents have enough resources
-// there will only be one round. However if there are any labs remaining it will try to distribute the remaining labs
-// to agents which are not resource capped, hence the full value in the agentLabDistribution type. If there are labs remaining
-// and all available agents are full, it will return an error and the requested event will not be started.
-//
-// Since we are working with floats, before we return the distribution we check if there are to many or to little labs.
-// In the case of to many labs, it will deduct labs which has recieved the least initialLabs then return the map.
-// In case of to little labs, the difference will be pushed to the labsRemainingToBeDistributed variable,
-// and more rounds will be run to compensate for the missing labs
-// Once everything fits, it will return the map
+// Returns a map of the lab distribution for available agents, based on how many labs should be spun up on each agent
 func (ap *AgentPool) calculateLabDistribution(agentsAvailable []*Agent, eventPool *EventPool, eventConfig EventConfig, resourceEstimates ResourceEstimates) (map[string]*agentLabDistribution, error) {
-	// ap.M.RLock()
-	// defer ap.M.RUnlock()
+	// Make sure the agents available are sorted by agent with largest weight first
+	sort.SliceStable(agentsAvailable, func(p, q int) bool {
+		return agentsAvailable[p].Weight > agentsAvailable[q].Weight
+	})
 
-	// Calculate the total weightSum of all available agents
-	var weightSum int32 = 0
-	for _, agent := range agentsAvailable {
-		weightSum += agent.Weight
-	}
-	log.Debug().Int32("WeightSum", weightSum).Msg("Calculated weightsum for available agents")
-
-	// Creating the actual map
+	//Prepare the map to return
 	agentLabDistributionMap := make(map[string]*agentLabDistribution)
-	// Creating the labsremaining counter
-	var labsRemainingToBeDistributed int32 = 0
-	for {
-		// If first round or there are labs remaining after the first round
-		if len(agentLabDistributionMap) == 0 || labsRemainingToBeDistributed > 0 {
-			allFull := true
 
-			// To keep track of how many labs was taken from remaining labs
-			// Has to be deducted at the end or else you cant correctly calculate the correct distribution of remaining labs
-			var labsTakenFromRemaining int32 = 0
+	labsToDistribute := eventConfig.MaxLabs
+Distributer:
+	for _, agent := range agentsAvailable {
+		if _, ok := agentLabDistributionMap[agent.Name]; !ok {
+			// First time running the distributer
+			log.Debug().Msg("agent not in lab distribution map")
+			agentLabDistributionMap[agent.Name] = &agentLabDistribution{
+				initialLabs: 0,
+				full:        false,
+			}
+		}
 
-			// When an agent is full we add its name to this slice to deduct its weight from the weightsum after each round
-			fullAgents := []string{}
-		Inner:
-			for _, agent := range agentsAvailable {
-				log.Debug().Int32("weightSum", weightSum).Msgf("Float value of calculation: %f", float64(labsRemainingToBeDistributed)*(float64(agent.Weight)/float64(weightSum)))
-				// By default we calculate the agent's initial labs from the remaining labs
-				// unless it is the first round
-				agentInitialLabs := int32(math.Round(float64(labsRemainingToBeDistributed) * (float64(agent.Weight) / float64(weightSum))))
-				agentDistribution, agentExists := agentLabDistributionMap[agent.Name]
-				if !agentExists {
-					log.Debug().Msg("First time")
-					agentLabDistributionMap[agent.Name] = &agentLabDistribution{
-						initialLabs: 0,
-						full:        false,
-					}
-					log.Debug().Msgf("Float value of calculation: %f", float64(eventConfig.MaxLabs)*(float64(agent.Weight)/float64(weightSum)))
-					agentInitialLabs = int32(math.Round(float64(eventConfig.MaxLabs) * (float64(agent.Weight) / float64(weightSum))))
-				} else {
-					// Ignore agent if it has no more resources
-					if agentDistribution.full {
-						continue Inner
-					}
-				}
-				// Edge case
-				if labsRemainingToBeDistributed > 0 && agentInitialLabs == 0 {
-					agentInitialLabs = labsRemainingToBeDistributed
-					labsRemainingToBeDistributed = 0
-				}
-				log.Debug().Int32("labsRemaining", labsRemainingToBeDistributed).Msg("Labs remaining")
-				log.Debug().Str("agentName", agent.Name).Int32("initialLabs", agentInitialLabs).Msg("InitialLabs for agent")
+		// In case agent has previously been assigned labs but do not have more resources.
+		if agentLabDistributionMap[agent.Name].full {
+			continue Distributer
+		}
+		currentEstimatedLabConsumption := agent.calculateCurrentEstimatedMemConsumption(eventPool)
 
-				currentEstimatedLabConsumption := agent.calculateCurrentEstimatedMemConsumption(eventPool)
-				log.Debug().Str("agentName", agent.Name).Uint64("currentEstimatedLabConsumption", currentEstimatedLabConsumption).Msg("Current summed memory usage estimation for agent")
+		// Weight determines how many labs will be spun up before continuing to next agent
+		for i := 0; i < int(agent.Weight); i++ {
+			newMemoryConsumptionEstimate := uint64(agentLabDistributionMap[agent.Name].initialLabs)*resourceEstimates.EstimatedMemUsagePerLab + resourceEstimates.EstimatedMemUsagePerLab + currentEstimatedLabConsumption
+			estimatedMemoryLeftOnAgent := agent.Resources.MemoryInstalled - newMemoryConsumptionEstimate
+			// Check if memory of new lab will surpass threshholds
+			if newMemoryConsumptionEstimate > agent.Resources.MemoryInstalled || estimatedMemoryLeftOnAgent < MemoryThreshHold*1000000000 {
+				agentLabDistributionMap[agent.Name].full = true
+				log.Debug().Str("agent", agent.Name).Msg("lab will surpass memory available")
+				log.Debug().Str("agent", agent.Name).Int32("labs", agentLabDistributionMap[agent.Name].initialLabs).Msg("labs distributed to agent")
+				continue Distributer
+			}
 
-				// When adding remaining labs to an agent we have to take the estimated memory usage of labs already
-				// added in the previous rounds into account
-				initialLabsEstimatedLabUsage := uint64(agentLabDistributionMap[agent.Name].initialLabs) * resourceEstimates.EstimatedMemUsagePerLab
-			InnerInner:
-				for i := 0; i < int(agentInitialLabs); i++ {
-					// For each lab keep increasing the memory consumption estimate until you reach the limit
-					// If the limit is not reached no labs will be pushed to the labsRemaining variable
-					newMemoryConsumptionEstimate := currentEstimatedLabConsumption + initialLabsEstimatedLabUsage + resourceEstimates.EstimatedMemUsagePerLab*uint64(i+1)
-
-					// Since we are substracting this variable from the memory installed value of the agent further down
-					// We have to make sure it is not actually bigger and will cause and interger overflow
-					if newMemoryConsumptionEstimate > agent.Resources.MemoryInstalled {
-						log.Debug().Str("agentName", agent.Name).Int32("initialLabs", agentLabDistributionMap[agent.Name].initialLabs).Int32("labsForRemainder", agentInitialLabs-int32(i)).Msg("agent memory full")
-						agentLabDistributionMap[agent.Name].full = true
-						fullAgents = append(fullAgents, agent.Name)
-						labsRemainingToBeDistributed += agentInitialLabs - int32(i)
-						break InnerInner
-					} else {
-						estimatedMemoryLeft := agent.Resources.MemoryInstalled - newMemoryConsumptionEstimate
-						// Checks if this lab will make the memory usage pass the threshold
-						if estimatedMemoryLeft < MemoryThreshHold*1000000000 {
-							log.Debug().Str("agentName", agent.Name).Int32("initialLabs", agentLabDistributionMap[agent.Name].initialLabs).Int32("labsForRemainder", agentInitialLabs-int32(i)).Msg("lab will surpass memory usage threshhold")
-							agentLabDistributionMap[agent.Name].full = true
-							fullAgents = append(fullAgents, agent.Name)
-							labsRemainingToBeDistributed += agentInitialLabs - int32(i)
-							break InnerInner
+			// All checks passed, add the lab to the agent
+			agentLabDistributionMap[agent.Name].initialLabs += 1
+			labsToDistribute -= 1
+			if labsToDistribute == 0 {
+				for _, agent := range agentsAvailable {
+					if _, ok := agentLabDistributionMap[agent.Name]; !ok {
+						// Make sure agent exists in map even though it has not been assigned any labs
+						agentLabDistributionMap[agent.Name] = &agentLabDistribution{
+							initialLabs: 0,
+							full:        false,
 						}
-						agentLabDistributionMap[agent.Name].initialLabs += 1
-						if agentExists {
-							labsTakenFromRemaining += 1
-						}
-					}
-				}
-				// If the agent is still not full after it has been assigned the labs
-				// All agents are not capped
-				if !agentLabDistributionMap[agent.Name].full {
-					allFull = false
-				}
-				//time.Sleep(1 * time.Second) // Debugging purposes
-			}
-
-			// If any agents went capped during the round, deduct their weight from the weightsum
-			for _, agentName := range fullAgents {
-				agent, err := ap.getAgent(agentName)
-				if err != nil {
-					return nil, errors.New("could not find agent in agentPool when deducting weight from sum")
-				}
-				weightSum -= agent.Weight
-				log.Debug().Str("agent", agent.Name).Int32("newSum", weightSum).Msg("New weightsum since some agents are full")
-			}
-
-			// Substract the labstaken from labs remaining
-			labsRemainingToBeDistributed -= labsTakenFromRemaining
-
-			// In case all labs are capped but there are still labsremaining to be assigned
-			if labsRemainingToBeDistributed > 0 && allFull {
-				return nil, NoResourcesError
-			}
-		} else {
-			var numberOfLabsDistributed int32 = 0
-			for _, distribution := range agentLabDistributionMap {
-				numberOfLabsDistributed += distribution.initialLabs
-			}
-
-			// The following is to recover from any loss or gain in labs from rounding
-			if numberOfLabsDistributed > eventConfig.MaxLabs {
-				var min string
-				first := true
-				// Finding the agent with least amount of labs assigned is also the agent with least ram available or least weight
-				for agentName, distribution := range agentLabDistributionMap {
-					if first {
-						min = agentName
-						first = false
 						continue
 					}
-					if distribution.initialLabs < agentLabDistributionMap[min].initialLabs {
-						min = agentName
-					}
+					log.Debug().Str("agent", agent.Name).Bool("full", agentLabDistributionMap[agent.Name].full).Int32("labs distributed", agentLabDistributionMap[agent.Name].initialLabs).Msg("labs distributed to agent")
 				}
-				numberOfToManyLabs := numberOfLabsDistributed - eventConfig.MaxLabs
-				log.Debug().Str("agent", min).Int32("labsOverMax", numberOfToManyLabs).Msg("to many labs distributed, deducting from agent with least resources")
-				agentLabDistributionMap[min].initialLabs -= numberOfToManyLabs
-			} else if numberOfLabsDistributed < eventConfig.MaxLabs {
-				// We need more labs, so add to labsRemaining and run 1 more cycle
-				labsRemainingToBeDistributed = eventConfig.MaxLabs - numberOfLabsDistributed
-				log.Debug().Int32("labsRemaining", labsRemainingToBeDistributed).Msg("Not anough labs distributed, pushing labs into labsremaining")
-				continue
+				return agentLabDistributionMap, nil
 			}
-			return agentLabDistributionMap, nil
 		}
 	}
 
+	if labsToDistribute > 0 {
+		allFull := true
+		for _, agent := range agentsAvailable {
+			if !agentLabDistributionMap[agent.Name].full {
+				allFull = false
+			}
+		}
+		if allFull {
+			return nil, NoResourcesError
+		}
+		goto Distributer
+	}
+
+	for _, agent := range agentsAvailable {
+		log.Debug().Str("agent", agent.Name).Bool("full", agentLabDistributionMap[agent.Name].full).Int32("labs distributed", agentLabDistributionMap[agent.Name].initialLabs).Msg("labs distributed to agent")
+	}
+	return agentLabDistributionMap, nil
 }
 
 // Closes a specific environment on all agents
